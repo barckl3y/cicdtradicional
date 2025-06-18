80c5cacffddd2684cece6bcc6f418042eed681d2
Christian Espinosa <barckl3y@PolypusServer.local>
2025-06-18 13:07:27 -0500

    FLUJO DE CI / CD CON GITHUB ACTIONS UNIFICADO v2

--- landingpage/tests.py
+++ landingpage/tests.py
@@ -8,4 +8,4 @@ class BasicTests(TestCase):
 
     def test_homepage_content(self):
         response = self.client.get(reverse('hola_mundo'))
-        self.assertContains(response, "<h1>HOLA MUNDO CON CI/CD CON GITHUB ACTIONS 18 de junio 2025 -v1</h1>")
+        self.assertContains(response, "<h1>HOLA MUNDO CON CI/CD CON GITHUB ACTIONS 18 de junio 2025 -v2</h1>")
