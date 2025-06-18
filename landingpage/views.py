b407c8619a0df2434744178fb4936707e0f5d6dc
Christian Espinosa <barckl3y@PolypusServer.local>
2025-06-18 12:42:35 -0500

    FLUJO DE CI / CD CON GITHUB ACTIONS

--- landingpage/views.py
+++ landingpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hola_mundo(request):
-    return HttpResponse("<h1>HOLA MUNDO CON CI/CD CON GITHUB ACTIONS</h1>")
+    return HttpResponse("<h1>HOLA MUNDO CON CI/CD CON GITHUB ACTIONS 18 de junio 2025</h1>")
